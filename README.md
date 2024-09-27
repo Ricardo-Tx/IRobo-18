2022a38a200baca25746fabbf8209e898f32c22d
Ricardo-Tx <ract2004@gmail.com>
2024-09-27 18:10:06 +0100

    updater launch and config files + more README info

--- README.md
+++ README.md
@@ -2,18 +2,45 @@
 
 ## Guide
 
+### What to do with `ekf_config.yaml` ?
+
+The file should be copied to the directory `<workspace>/src/robot_localization/params/`. It is accessed by the `turtlebot3_playbag.launch` launch file dynamically at this location.
+
+### What to do with `turtlebot3_playbag.launch` ?
+
+The file should be copied to `<workspace>/src/turtlebot3_datasets/launch/` in order for the command 
+```bash
+roslaunch turtlebot3_datasets turtlebot3_playbag.launch
+``` 
+to work. It depends on `ekf_config.yaml` being in the right location and a RViz config file `rosbag.rviz` being available at the location `<workspace>/src/turtlebot3_datasets/data/`. Tweak the file locations in the launch file as necessary.
+
+### What to do with the matrix transformation in the [turtlerobot3_datasets](https://github.com/irob-labs-ist/turtlebot3_datasets) package?
+
+The transformation is strictly equal to that in `publish_initial_tf.sh`. The first 3 digits are the translation *(x, y, z)* and the last 4 are the rotation quaternion *(x, y, z, w)*.
+
+### Running `source devel/setup.bash` constantly is boring
+Copy `catkin_daemon.sh` to your home directory (adjacent to `.bashrc`).
+Run
+```bash
+chmod +x catkin_daemon.sh
+```
+to make it executable. Then append the following line to `.bashrc`:
+```bash
+source catkin_daemon.sh
+```
+Now everytime you `cd` into a catkin workspace you will be prompted to automatically set it up if it's not already setup. It also warns about libraries without binaries in `devel/lib/` that need to be compiled with `catkin_make`.
+
 ### How to run `.launch` files?
 Either `cd` into the location of the file and run
-```
+```bash
 roslaunch my-launch.launch
 ``` 
 or get the file into a `<pkg>/launch/` folder and run
-```
+```bash
 roslaunch <pkg> my-launch.launch
 ```
 
 ### How to make gazebo not show GUI when running `roslaunch turtlebot3_gazebo turtlebot3_world.launch` ?
-
 Run
 ```bash
 cd `rospack find turtlebot3_gazebo`/launch
--- catkin_daemon.sh
+++ catkin_daemon.sh
@@ -6,18 +6,42 @@ cd() {
     if [ -f ".catkin_workspace" ]; then
 
         CWD=$(pwd)/src
-        if [[ ":$ROS_PACKAGE_PATH:" == *":$CWD:"* ]]; then
-            # Current workspace has already been setup
-            return
+        DEVEL=$(pwd)/devel 
+
+        if [[ ":$ROS_PACKAGE_PATH:" != *":$CWD:"* ]]; then
+            # Current workspace has not been setup
+            echo "This Catkin workspace ($CWD) is NOT in ROS_PACKAGE_PATH!"
+            echo "Do you want to source this workspace? (y/n)"
+            read -r response
+
+            # Check if the response is 'y' or 'Y'
+            if [[ "$response" == "y" || "$response" == "Y" ]]; then
+                source devel/setup.bash
+            fi
         fi
 
-        echo "This Catkin workspace ($CWD) is NOT in ROS_PACKAGE_PATH!"
-        echo "Do you want to source this workspace? (y/n)"
-        read -r response
 
-        # Check if the response is 'y' or 'Y'
-        if [[ "$response" == "y" || "$response" == "Y" ]]; then
-            source devel/setup.bash
-        fi
+        for PKG_DIR in "$CWD"/*; do
+            if [ -d "$PKG_DIR" ]; then
+                # Get the package name (which is the directory name)
+                PKG_NAME=$(basename "$PKG_DIR")
+
+                # Check if there are any binaries or libraries in the devel/lib folder
+                LIB_DIR="$DEVEL/lib/$PKG_NAME"
+                INCLUDE_DIR="$DEVEL/include/$PKG_NAME"
+                SHARE_DIR="$DEVEL/share/$PKG_NAME"
+                
+                # Check if binaries or other install files exist
+                # if [ -d "$BIN_DIR" ] || [ -d "$INCLUDE_DIR" ] || [ -d "$SHARE_DIR" ]; then
+                #     echo "Package $PKG_NAME has binaries or install files."
+                # else
+                #     echo "Package $PKG_NAME has NO binaries installed."
+                # fi
+                if [ ! -d "$LIB_DIR" ] || [ -z "$(ls -A "$LIB_DIR")" ]; then
+                    echo "!!! Package $PKG_NAME has NO binaries installed."
+                fi
+            fi
+        done
+        
     fi
 }

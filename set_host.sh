6d39549b18703233ad054b2168f2645dfa3a644c
Ricardo-Tx <ract2004@gmail.com>
2024-10-18 01:46:34 +0100

    copy path publisher

--- set_host.sh
+++ set_host.sh
@@ -19,10 +19,17 @@ read -p "Enter your choice [0-5]: " choice
 if [[ $choice -eq 0 ]]; then
     TURTLEBOT3_IP="localhost"  # Localhost
     TURTLEBOT3_NUMBER="$BASE_NUMBER"
+
+    export ROS_HOSTNAME="localhost"
+    export ROS_IP="localhost"
 else
     TURTLEBOT3_IP="${BASE_IP}$(($BASE_NUMBER + $choice))"  # TurtleBot3 IPs from 11 to 15
     TURTLEBOT3_NUMBER="$(($BASE_NUMBER + $choice))"
     TURTLEBOT3_NAME="waffle$choice"
+
+    IP_ADDRESS="172.19.228.77"
+    export ROS_HOSTNAME="$IP_ADDRESS"
+    export ROS_IP="$IP_ADDRESS"
 fi
 
 # Set the environment variables
@@ -32,8 +39,5 @@ export TURTLEBOT3_NUMBER
 export ROS_MASTER_URI="http://${TURTLEBOT3_IP}:11311"
 
 # IP_ADDRESS=$(hostname -I | awk '{print $1}')
-IP_ADDRESS="172.19.228.77"
-export ROS_HOSTNAME="$IP_ADDRESS"
-export ROS_IP="$IP_ADDRESS"
 
 echo "Environment variables set for TurtleBot3 with IP: $TURTLEBOT3_IP"
